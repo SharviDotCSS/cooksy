2187dd4c5337fbf5cdef94babf710466e0be12c0
Sharvi Belsare <130642118+SharviDotCss@users.noreply.github.com>
2025-04-18 20:52:03 +0530

    hotfixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import AddRecipe from "./pages/AddRecipe";
-import SearchRecipe from "./pages/SearchRecipe";
+// import SearchRecipe from "./pages/SearchRecipe";
 import Login from "./pages/Login";
 import Signin from "./pages/Signup";
 import UserDashboard from "./pages/UserDashboard";
-import Admin from "./pages/Admin";
+// import Admin from "./pages/Admin";
 import RecipeInfo from "./pages/RecipeInfo";
 import Signup from "./pages/Signup";
 
@@ -15,11 +15,11 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/add-recipe" element={<AddRecipe />} />
-        <Route path="/search" element={<SearchRecipe />} />
+        {/* <Route path="/search" element={<SearchRecipe />} /> */}
         <Route path="/login" element={<Login />} />
         <Route path="/signin" element={<Signin />} />
         <Route path="/dashboard" element={<UserDashboard />} />
-        <Route path="/admin" element={<Admin />} />
+        {/* <Route path="/admin" element={<Admin />} /> */}
         <Route path="/recipe/:id" element={<RecipeInfo />} />
         <Route path="/register" element={<Signup />} />
       </Routes>
